8c50b469edd76dcfb7c49b7a3a21a5c4e3e483be
mashi2must <mashi2must@gmail.com>
2021-11-14 16:47:55 +0900

    ogpのURL変更

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -7,7 +7,7 @@ const emojify = (text: string) => twemoji.parse(text, twOptions);
 
 function getCss(theme: string, fontSize: string) {
   let background = "white";
-  let background_img_url = "https://site-groupie.vercel.app/ogp/ogp.png";
+  let background_img_url = "https://groupie.tokyo/ogp/ogp.png";
 
   if (theme === "dark") {
     background = "black";
